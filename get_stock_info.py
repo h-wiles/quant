761d0dcc92fab6dd0016c45fe7101e4a4d99e78e
HUANG-XJUP <2190677233@qq.com>
2025-12-25 21:47:21 +0800

    # feat: get stock info such as code_name etc.

---
+++ get_stock_info.py
@@ -0,0 +1,30 @@
+import baostock as bs
+import pandas as pd
+
+
+lg = bs.login()
+# 显示登陆返回信息
+print('login respond error_code:' + lg.error_code)
+print('login respond  error_msg:' + lg.error_msg)
+
+
+rs = bs.query_history_k_data_plus("sh.600000",
+                                  "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,isST",
+                                  start_date='2025-07-01', end_date='2025-12-31',
+                                  frequency="d", adjustflag="3")
+print('query_history_k_data_plus respond error_code:' + rs.error_code)
+print('query_history_k_data_plus respond  error_msg:' + rs.error_msg)
+
+
+data_list = []
+while (rs.error_code == '0') & rs.next():
+    # 获取一条记录，将记录合并在一起
+    data_list.append(rs.get_row_data())
+result = pd.DataFrame(data_list, columns=rs.fields)
+
+
+result.to_csv("D:\\history_A_stock_k_data.csv", index=False)
+print(result)
+
+
+bs.logout()
